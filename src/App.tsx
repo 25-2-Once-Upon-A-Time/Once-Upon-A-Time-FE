988250bb81e81a9cbf36d5f526dd18d4e5f56568
gkrrywhtk2 <rlatmdrl4512@daum.net>
2025-11-25 16:07:05 +0900

    feat : 수정사항 반영

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
-import ButtonTestPage from '@/pages/ButtonTestPage';
+import ButtonTestPage from '@/pages/test/ButtonTestPage';
 
 function App() {
   return (
--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -10,19 +10,15 @@ const Button: React.FC<ButtonProps> = ({
   className,
   ...props
 }) => {
-  // 기본 스타일
+  // 기본 스타일 (모든 버튼 공통)
   const baseStyles =
-    'px-[16px] h-[52px] rounded-[8px] text-[#FFFFFF] text-[20px] font-montserrat font-semibold transition-all';
+    'px-[16px] h-[52px] rounded-[8px] text-[20px] font-montserrat font-semibold transition-all';
 
   // 자주 쓰는 variant 스타일을 저장
   const variantStyles: Record<string, string> = {
-    primary: 'bg-[#3C3C5E] text-[#FFFFFF] text-[20px] hover:bg-[#5F6380] rounded-[8px]',
-    secondary: 'bg-[#DCE3E8] text-[#333D4B] hover:bg-[#CDD4D9]',
-    back: 'bg-transparent text-[#545873] hover:bg-[#F5F5F5]',
+    primary: 'bg-[#3C3C5E] text-[#FFFFFF] hover:bg-[#5F6380]',
+    back: 'bg-transparent text-[#545873] hover:bg-[#F5F5F5] hover:text-[#3C3C5E]',
   };
-
-  // 터치 막아두는 스타일
-  const disabledStyles = disabled ? 'bg-[#E9EEF2] text-[#CDC6DD] cursor-not-allowed' : '';
 
   // variant가 있으면 미리 정의된 스타일, 없으면 빈 문자열 -> 유연하게 작동되게 해보았습니다.
   const selectedVariant = variant ? variantStyles[variant] : '';
@@ -30,13 +26,7 @@ const Button: React.FC<ButtonProps> = ({
   return (
     <button
       disabled={disabled}
-      className={twMerge(
-        baseStyles,
-        selectedVariant,
-        disabledStyles,
-        fullWidth && 'w-full',
-        className,
-      )}
+      className={twMerge(baseStyles, selectedVariant, fullWidth && 'w-full', className)}
       {...props}
     >
       {children}
