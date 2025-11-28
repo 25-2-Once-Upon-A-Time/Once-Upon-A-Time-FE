4cce22bf14ce03abbcecd21e5ffb53970e499d22
eun-seoo <163312948+eun-seoo@users.noreply.github.com>
2025-11-28 23:46:12 +0900

    chore(test): Input, Tag 컴포넌트 테스트 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import ButtonTestPage from '@/pages/test/ButtonTestPage';
+import InputTestPage from '@/pages/test/InputTestPage';
+import SearchInputTestPage from '@/pages/test/SearchInputTestPage';
+import TagTestPage from '@/pages/test/TagTestPage';
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/test/button' element={<ButtonTestPage />} />
+        <Route path='/test/input' element={<InputTestPage />} />
+        <Route path='/test/search' element={<SearchInputTestPage />} />
+        <Route path='/test/tag' element={<TagTestPage />} />
       </Routes>
     </BrowserRouter>
   );
