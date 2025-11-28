501e4f505c47ddea8ddfc3018d85520bd7c7a11c
eun-seoo <163312948+eun-seoo@users.noreply.github.com>
2025-11-28 22:35:52 +0900

    refactor: 테스트 페이지 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
-import ButtonTestPage from '@/pages/test/ButtonTestPage';
-import ToastTestPage from '@/pages/test/ToastTestPage';
-import DownloadToastTestPage from '@/pages/test/DownloadToastTestPage';
-import ErrorToastTestPage from '@/pages/test/ErrorToastTestPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/test/button' element={<ButtonTestPage />} />
-        <Route path='/test/toast' element={<ToastTestPage />} />
-        <Route path='/test/download-toast' element={<DownloadToastTestPage />} />
-        <Route path='/test/error-toast' element={<ErrorToastTestPage />} />
       </Routes>
     </BrowserRouter>
   );
