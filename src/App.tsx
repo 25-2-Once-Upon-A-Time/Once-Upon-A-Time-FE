2ae47b6c044df9b7ca4905cd25f2abad20a60b4e
eun-seoo <163312948+eun-seoo@users.noreply.github.com>
2025-11-29 01:54:59 +0900

    chore: 이미지 컴포넌트 테스트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import ButtonTestPage from '@/pages/test/ButtonTestPage';
+import ImageCardTestPage from '@/pages/test/ImageCardTestPage';
+import ImageTestPage from '@/pages/test/ImageTestPage';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/test/button' element={<ButtonTestPage />} />
+        <Route path='/test/image-card' element={<ImageCardTestPage />} />
+        <Route path='/test/image' element={<ImageTestPage />} />
       </Routes>
     </BrowserRouter>
   );
