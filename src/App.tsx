0533c83e0abf7444c233d8d70dfa06402f2f8a27
eun-seoo <163312948+eun-seoo@users.noreply.github.com>
2025-11-28 21:20:19 +0900

    chore: 테스트 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import ButtonTestPage from '@/pages/test/ButtonTestPage';
+import ToastTestPage from '@/pages/test/ToastTestPage';
+import DownloadToastTestPage from '@/pages/test/DownloadToastTestPage';
+import ErrorToastTestPage from '@/pages/test/ErrorToastTestPage';
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/test/button' element={<ButtonTestPage />} />
+        <Route path='/test/toast' element={<ToastTestPage />} />
+        <Route path='/test/download-toast' element={<DownloadToastTestPage />} />
+        <Route path='/test/error-toast' element={<ErrorToastTestPage />} />
       </Routes>
     </BrowserRouter>
   );
