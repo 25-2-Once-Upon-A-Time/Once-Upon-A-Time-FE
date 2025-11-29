814e35fb1a9d60db6d8949a4baad01c9f8bbba55
gkrrywhtk2 <rlatmdrl4512@daum.net>
2025-11-29 22:32:48 +0900

    chore : 충돌 최소화

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
-import ButtonTestPage from '@/pages/test/ButtonTestPage';
-import TextButtonTestPage from '@/pages/test/TextButtonTestPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/test/button' element={<ButtonTestPage />} />
-        <Route path='/test/textbutton' element={<TextButtonTestPage />} />
       </Routes>
     </BrowserRouter>
   );
