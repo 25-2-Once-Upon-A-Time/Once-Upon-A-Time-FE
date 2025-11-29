cab1ea75a2bd8a1105f33c64250a6285b4f34e81
gkrrywhtk2 <rlatmdrl4512@daum.net>
2025-11-29 22:27:18 +0900

    App.tsx 충돌 최소화

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import ButtonTestPage from '@/pages/test/ButtonTestPage';
-import CharacterPage from '@/pages/CharacterPage';
-import StoryPage from '@/pages/StoryPage';
-import CommunityPage from '@/pages/CommunityPage';
-import MyPage from '@/pages/MyPage';
-import { BottomNav } from '@/components/BottomNav';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='pb-[80px]'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/test/button' element={<ButtonTestPage />} />
-          <Route path='/character' element={<CharacterPage />} />
-          <Route path='/story' element={<StoryPage />} />
-          <Route path='/community' element={<CommunityPage />} />
-          <Route path='/mypage' element={<MyPage />} />
-        </Routes>
-        <BottomNav />
-      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/test/button' element={<ButtonTestPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
