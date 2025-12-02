a3bb111957199e073c1d0c829f9a2719ace37e3f
eun-seoo <163312948+eun-seoo@users.noreply.github.com>
2025-12-02 21:49:52 +0900

    chore: 반응형 레이아웃으로 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import Button from '@/components/ui/Button';
-import { TopNav } from '@/components/TopNav';
+import { TopNav } from '@/components/ui/TopNav';
 
 export default function Home() {
   const navigate = useNavigate();
