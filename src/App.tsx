82da7f44bf4888cddec0a5fed27a32c7c2d3619c
eun-seoo <163312948+eun-seoo@users.noreply.github.com>
2025-12-04 12:55:54 +0900

    feat(character): 캐릭터 페이지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
+import CharacterListPage from '@/pages/character/CharacterListPage';
+import CharacterDetailPage from '@/pages/character/CharacterDetailPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/character' element={<CharacterListPage />} />
+        <Route path='/character/:id' element={<CharacterDetailPage />} />
       </Routes>
     </BrowserRouter>
   );
