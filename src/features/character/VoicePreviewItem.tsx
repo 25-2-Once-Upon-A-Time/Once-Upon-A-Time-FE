fb0936f5d907c38bb1f77d148720897a4352fd00
eun-seoo <163312948+eun-seoo@users.noreply.github.com>
2025-12-04 13:35:41 +0900

    refactor(character): VoicePreviewItem에 twMerge 적용

--- src/features/character/VoicePreviewItem.tsx
+++ src/features/character/VoicePreviewItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { twMerge } from 'tailwind-merge';
 import PlayButton from '@/components/ui/PlayButton/PlayButton';
 
 interface VoicePreviewItemProps {
@@ -19,14 +20,12 @@ const VoicePreviewItem: React.FC<VoicePreviewItemProps> = ({
   style,
 }) => {
   return (
-    <div className={`h-[40px] flex items-center gap-[20.44px] ${className || ''}`} style={style}>
+    <div className={twMerge('h-[40px] flex items-center gap-[20.44px]', className)} style={style}>
       <PlayButton isPlaying={isPlaying} onClick={onPlayChange} />
 
       <div className='flex flex-col flex-shrink-0'>
-        {/* 음성 타이틀 */}
         <p className='nsr-16-eb text-fg-cream h-[17px] whitespace-nowrap'>{title}</p>
 
-        {/* 음성 시간 */}
         <p className='pre-11-m text-fg-cream h-[12px] mt-[5.9px] whitespace-nowrap'>{duration}</p>
       </div>
     </div>
