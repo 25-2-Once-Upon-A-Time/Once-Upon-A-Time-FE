ffe3a9edc3022f52f2deb93d2fe87b64a2be7a66
eun-seoo <163312948+eun-seoo@users.noreply.github.com>
2025-12-04 12:48:10 +0900

    refactor(image): 이미지 컴포넌트에 반응형 레이아웃 적용

--- src/components/ui/Image/Image.tsx
+++ src/components/ui/Image/Image.tsx
@@ -5,11 +5,13 @@ import { twMerge } from 'tailwind-merge';
 
 const Image: React.FC<ImageProps> = ({ src, alt = 'Image', className }) => {
   return (
-    <div className={twMerge('w-[412px] h-[240px] bg-[#E5E5E5] flex items-center justify-center', className)}>
+    <div
+      className={twMerge('w-full h-auto bg-[#E5E5E5] flex items-center justify-center', className)}
+    >
       {src ? (
-        <img src={src} alt={alt} className="w-full h-full object-cover" />
+        <img src={src} alt={alt} className='w-full h-full object-cover' />
       ) : (
-        <ImagePlaceholder className="w-[32px] h-[32px]" />
+        <ImagePlaceholder className='w-[32px] h-[32px]' />
       )}
     </div>
   );
