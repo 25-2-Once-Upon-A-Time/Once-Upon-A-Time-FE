32d22e086ed2102e42b0819fe4088695e3bc1f3b
eun-seoo <163312948+eun-seoo@users.noreply.github.com>
2025-12-05 00:34:41 +0900

    refactor: 불필요한 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '@/pages/Home';
 import CharacterListPage from '@/pages/character/CharacterListPage';
 import CharacterDetailPage from '@/pages/character/CharacterDetailPage';
 import MyPage from '@/pages/mypage/MyPage';
@@ -8,7 +7,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
         <Route path='/character' element={<CharacterListPage />} />
         <Route path='/character/:id' element={<CharacterDetailPage />} />
         <Route path='/mypage' element={<MyPage />} />
