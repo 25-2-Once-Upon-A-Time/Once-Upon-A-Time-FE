488e0714d652535091ae97a252b4ffebd2d73645
eun-seoo <163312948+eun-seoo@users.noreply.github.com>
2025-12-07 18:50:48 +0900

    feat(audiobook): 오디오북 데이터 상수 추가

---
+++ src/constants/audiobooks.ts
@@ -0,0 +1,25 @@
+export interface AudioBook {
+  id: number;
+  title: string;
+  tags: string[];
+  character: string;
+  time: string;
+  imageSrc?: string;
+}
+
+export const audiobooks: AudioBook[] = [
+  {
+    id: 1,
+    title: '나눠먹으면 맛있어요',
+    tags: ['#용기', '#따뜻한'],
+    character: '뽀로로',
+    time: '00:01:32',
+  },
+  {
+    id: 2,
+    title: '나눠먹으면 맛있어요',
+    tags: ['#모험', '#신비로움'],
+    character: '신데렐라',
+    time: '00:01:32',
+  },
+];
